0f589b4cb18bdc1fc3c56a89a8ce85c750b32111
Susaid8 <sumayasaid15@icloud.com>
2022-05-10 08:06:46 -0500

    almost done

--- index.js
+++ index.js
@@ -3,8 +3,27 @@ const searchResultDiv = document.querySelector('.search-result')
 const container = document.querySelector('.container')
 let searchQuery = ''
 
+const submitForm = document.getElementById('submitForm')
+
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault()
     searchQuery = e.target.querySelector('input').value
-    console.log(searchQuery)
+    // console.log(searchQuery)
 })
+
+submitForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    // console.log('Form has been submitted!!')
+
+    const request = new XMLHttpRequest()
+
+    request.open('post', 'contact.php')
+    request.onload = function () {
+        console.log(request.responseText)
+    }
+
+    request.send(new FormData (submitForm))
+
+})
+
+
